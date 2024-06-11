ac8fcd7c5a54a589bef0969a9e4547c9d39f48d9
kirankab7 <kirankab7@gmail.com>
2024-06-11 19:56:24 +0530

    Simple App

---
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.fragments
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.fragments.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
